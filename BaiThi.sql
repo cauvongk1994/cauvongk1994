2b32b8d79967a582b9199a77b3629d9df5eacc7d
cauvongk1994 <81789929+cauvongk1994@users.noreply.github.com>
2021-07-26 09:11:10 +0700

    Baithi.SQL

---
+++ BaiThi.sql
@@ -0,0 +1,26 @@
+﻿CREATE DATABASE KiemTra
+go
+USE KiemTra
+GO
+CREATE TABLE V_Parts(
+	PartID int identity(1,1) primary key,
+	PartName nvarchar(100),
+	CateID char(6),
+	Description nvarchar(1000)
+)
+GO
+INSERT INTO V_Parts(
+	CateName, 
+	CateID,
+	Photo,
+)
+Values (
+	N'Nguyễn Văn A',',
+	CPU,HDD,RAM'
+),
+(
+	N'Nguyễn Văn B',
+	',CPU,HDD,RAM'
+)
+go
+SELECT * FROM V_Parts WHERE V_Parts Like '%100%'
